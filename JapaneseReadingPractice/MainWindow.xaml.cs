d4cfb5c752a5afe4be3022c2e2768b7b956df54e
icemasterpt <biogeek1337@gmail.com>
2019-03-12 07:34:25 +0000

    First release. You generate a string in Hiragana and you will have to write it in romanji as fast as possible. Your time and your success rate will be stored during the current session only.

---
+++ JapaneseReadingPractice/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace JapaneseReadingPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+  
+    public partial class MainWindow : Window
+    {
+
+        public Timer t1 = new Timer();
+        public hiraControl hiraganaC;
+        public String result = "";
+        public String resultRomanji = "";
+        public int currentTime = 0;
+
+        //Controller Vars
+        public int LastTime = 0;
+        public float totalTime = 0.00001f;
+        public int TotalTries = 0;
+        public int NumCorrect = 0;
+        private bool inGame = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            hiraganaC = new hiraControl();
+            if (TotalTries > 0)
+                lbScreen_Copy.Content = "Stats: Answer Correct :" + NumCorrect.ToString() + " Success Rate: " + (NumCorrect / TotalTries).ToString() + " average time: " + (totalTime / TotalTries).ToString();
+            else
+                lbScreen_Copy.Content = "Stats: Answer Correct :" + NumCorrect.ToString() + " : Not enough data to create a statistic!";
+
+            t1.Tick += T1_Tick;
+            t1.Interval = 1000;
+        }
+
+        public bool performTest(String a, String b)
+        {
+            if (a == b)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (inGame)
+            {
+                //do nothing lol
+            }
+            else
+            {
+                //make it ingame
+                inGame = true;
+                //start the timer!
+                lbScreen.FontSize = 50;
+
+                t1.Start();
+
+                List<String> listOfStrings = hiraganaC.generateString(4);
+                lbScreen.Content = hiraganaC.fromHiraganaStringFromSyllabs(listOfStrings);
+                resultRomanji = hiraganaC.formStringFromSyllabs(listOfStrings);
+                result = hiraganaC.formStringFromSyllabs(listOfStrings);
+            }
+        }
+
+        private void T1_Tick(object sender, EventArgs e)
+        {
+            currentTime++;
+            lbStat.Content = currentTime.ToString();
+        }
+
+   
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            String resposta = InPutBox.Text.ToString();
+            
+
+            if (resposta.Equals(resultRomanji) && inGame)
+            {
+                t1.Stop();
+                lbStat.Content = "STOP!";
+                TotalTries++;
+                lbScreen.Content ="SUCCESS!! you took " + currentTime.ToString() + " seconds!";
+                totalTime += currentTime;
+                currentTime = 0;
+                lbStat.Content = currentTime.ToString();
+                InPutBox.Clear();
+                NumCorrect++;
+                inGame = false;
+            }
+            else if (inGame)
+            {
+                t1.Stop();
+               
+                lbStat.Content = "STOP!";
+                TotalTries++;
+                lbScreen.FontSize = 30;
+                lbScreen.Content="wrong " + currentTime.ToString() + " seconds!\n answer: "+ resultRomanji+"\n you said: "+resposta;
+                totalTime += currentTime;
+                currentTime = 0;
+                lbStat.Content = currentTime.ToString();
+                InPutBox.Clear();
+                inGame = false;
+            }
+            else if(!inGame)
+            {
+                MessageBox.Show("You must generate a question first!");
+            }
+            if (TotalTries > 0)
+                lbScreen_Copy.Content = "Stats: Answer Correct :" + NumCorrect.ToString() + " Success Rate: " + Convert.ToDouble((NumCorrect/TotalTries)*100).ToString("0.00") + " average time: " + Convert.ToInt16(totalTime / TotalTries).ToString("0.0");
+        }
+    }
+}
---
+++ JapaneseReadingPractice/hiraControl.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//controlo para ser usado apenas para Hiragana
+namespace JapaneseReadingPractice
+{
+    public class hiraControl
+    {
+        private Dictionary<String, String> hiraDict,hiraDictvowelsOnly, hiraDictAccents;
+        private List<String> ListOfSyllabs;
+        private Random random;
+
+        private List<String> generatedString;
+        public hiraControl()
+        {
+            random = new Random();
+            this.hiraDict = new Dictionary<String, String>();
+        
+            this.hiraDict.Add("a", "あ");
+            this.hiraDict.Add("e", "え");
+            this.hiraDict.Add("i", "い");
+            this.hiraDict.Add("o", "お");
+            this.hiraDict.Add("u", "う");
+            this.hiraDict.Add("ka", "か");
+            this.hiraDict.Add("ke", "け");
+            this.hiraDict.Add("ki", "き");
+            this.hiraDict.Add("ko", "こ");
+            this.hiraDict.Add("ku", "く");
+            this.hiraDict.Add("na", "な");
+            this.hiraDict.Add("ne", "ね");
+            this.hiraDict.Add("ni", "に");
+            this.hiraDict.Add("no", "の");
+            this.hiraDict.Add("nu", "ぬ");
+            this.hiraDict.Add("ma", "ま");
+            this.hiraDict.Add("me", "め");
+            this.hiraDict.Add("mi", "み");
+            this.hiraDict.Add("mo", "も");
+            this.hiraDict.Add("mu", "む");
+            this.hiraDict.Add("wa", "わ");
+            this.hiraDict.Add("wo", "を");
+            this.hiraDict.Add("sa", "さ");
+            this.hiraDict.Add("se", "せ");
+            this.hiraDict.Add("shi", "し");
+            this.hiraDict.Add("so", "そ");
+            this.hiraDict.Add("su", "す");
+            this.hiraDict.Add("ha", "は");
+            this.hiraDict.Add("he", "へ");
+            this.hiraDict.Add("hi", "ひ");
+            this.hiraDict.Add("ho", "ほ");
+            this.hiraDict.Add("hu", "ふ");
+            this.hiraDict.Add("ra", "ら");
+            this.hiraDict.Add("re", "れ");
+            this.hiraDict.Add("ri", "り");
+            this.hiraDict.Add("ro", "ろ");
+            this.hiraDict.Add("ru", "る");
+            this.hiraDict.Add("chi", "ち");
+
+            //sillabs
+
+            ListOfSyllabs = new List<string>();
+
+
+            ListOfSyllabs.Add("a");
+            ListOfSyllabs.Add("e" );
+            ListOfSyllabs.Add("i" );
+            ListOfSyllabs.Add("o" );
+            ListOfSyllabs.Add("u" );
+            ListOfSyllabs.Add("ka");
+            ListOfSyllabs.Add("ke");
+            ListOfSyllabs.Add("ki");
+            ListOfSyllabs.Add("ko");
+            ListOfSyllabs.Add("ku");
+            ListOfSyllabs.Add("wa");
+            ListOfSyllabs.Add("wo");
+            ListOfSyllabs.Add("ha");
+            ListOfSyllabs.Add("he");
+            ListOfSyllabs.Add("hi");
+            ListOfSyllabs.Add("ho");
+            ListOfSyllabs.Add("shi");
+            ListOfSyllabs.Add("chi");
+            ListOfSyllabs.Add("na");
+            ListOfSyllabs.Add("ne");
+            ListOfSyllabs.Add("ni");
+            ListOfSyllabs.Add("no");
+            ListOfSyllabs.Add("nu");
+            ListOfSyllabs.Add("ra");
+            ListOfSyllabs.Add("re");
+            ListOfSyllabs.Add("ri");
+            ListOfSyllabs.Add("ro");
+            ListOfSyllabs.Add("ru");
+
+            //setup new list of strings to be compared later during the main program
+            this.generatedString = new List<string>();
+
+
+        }
+
+        /*Generates a random string that will be used*/
+        private string getRandomSyllab()
+        {
+           
+            int ammountOfSyllabs = this.ListOfSyllabs.Count;
+            int index = random.Next(0, ammountOfSyllabs);
+            string syllab = this.ListOfSyllabs[index];
+
+
+
+            return syllab;
+        }
+
+        public List<String> generateString(int size)
+        {
+            //primeiro, apaga-se tudo o que se encontra na lista
+            this.generatedString.Clear();
+            List<String> strings = new List<string>();
+
+            for (int i = 0; i<=size; i++)
+            {
+                String k = getRandomSyllab();
+                strings.Add(k);
+
+            }
+            return strings;
+
+        }
+
+        public string formStringFromSyllabs(List<String> syllabs)
+        {
+            String FinalString = "";
+            foreach (String k in syllabs)
+            {
+                FinalString += k;
+            }
+            return FinalString;
+        }
+
+        public string fromHiraganaStringFromSyllabs(List<String> syllabs)
+        {
+            String FinalString = "";
+            foreach (String k in syllabs)
+            {
+                FinalString += hiraDict[k];
+            }
+            return FinalString;
+        }
+
+
+
+    }
+}
+
+
+
